7c0a1edb27d6e498f2a026b8841ac2582b6eb12d
Jared Sevilla <jaredgsevilla@gmail.com>
2024-07-15 20:38:48 -0700

    made maze bigger

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 class Settings:
     """Container for application variables."""
 
-    tile_size = 30
+    tile_size = 25
     wall_width = max(tile_size // 10, 1)
-    maze_size = (20, 20)
+    maze_size = (50, 50)
     color_empty  = (255, 255, 255)
     color_seen   = (  0, 255, 255)
     color_active = (255,   0,   0)
