8be4527d1892c99cd618edc5694a11064a6c9d57
Jared Sevilla <jaredgsevilla@gmail.com>
2024-07-15 21:03:10 -0700

    made maze bigger

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ class Settings:
 
     tile_size = 25
     wall_width = max(tile_size // 10, 1)
-    maze_size = (50, 50)
+    maze_size = (80, 50)
     color_empty  = (255, 255, 255)
     color_seen   = (  0, 255, 255)
     color_active = (255,   0,   0)
