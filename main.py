60bb38e91f09cbea70921a8655df67ab3f63ed68
Jared Sevilla <jaredgsevilla@gmail.com>
2024-07-15 20:38:25 -0700

    added search visualization

--- main.py
+++ main.py
@@ -4,17 +4,28 @@ pg.init()
 from settings import Settings
 from maze import Maze
 from walls import get_walls_overlay
+from tiles import Tiles, Tile
+from search import a_star
 
 
 class App:
     """Application class for graph search visualizer."""
     def __init__(self):
         self.screen = pg.display.set_mode(tuple(map(lambda x: x * Settings.tile_size, Settings.maze_size)))
+        self.clock = pg.time.Clock()
 
         self.maze = Maze(Settings.maze_size)
         self.wall_overlay = get_walls_overlay(self.maze, Settings.tile_size, self.screen.get_size())
+        self.tiles = Tiles(Settings.maze_size)
+        self.search = a_star(self.maze, 0, (self.maze.size[0]*self.maze.size[1])-1, self)
+        print(self.search.dest)
 
     def run(self):
+
+        self.tiles.draw(self.screen)
+        self.screen.blit(self.wall_overlay, (0, 0))
+        timer = 0
+        found = False
 
         while True:
             
@@ -24,11 +35,21 @@ class App:
                     pg.quit()
                     exit(0)
 
-            self.screen.fill(Settings.color_empty)
+                # if event.type == pg.KEYDOWN:
+                #     if event.key == pg.K_1:
+                #         self.search.step()
 
-            self.screen.blit(self.wall_overlay, (0, 0))
+            timer += self.clock.tick(60)
+            if not found and timer > .25:
+                if (self.search.step()): found = True
 
             pg.display.flip()
+
+    def change_tile(self, vertex, color):
+        tile = self.tiles[vertex]
+        Tiles.set_color(color, [tile])
+        tile.draw(self.screen)
+        self.screen.blit(self.wall_overlay, (0, 0))
 
 
 if __name__ == "__main__":
